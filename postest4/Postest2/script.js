fa74c549ac76097a666e25ed6d89f87cdfc76af9
IrfanArmanH <113760136+IrfanArmanH@users.noreply.github.com>
2022-10-14 09:01:10 +0800

    session login

---
+++ postest4/Postest2/script.js
@@ -0,0 +1,27 @@
+// darkmode start
+function ubahmode(){
+    const ubah = document.body;
+    ubah.classList.toggle("dark");
+
+}
+// darkmode end 
+
+// addeventlistener show image merk start
+const button = document.getElementById('btn-merk');
+button.addEventListener("click",function show_image() { 
+    const x = document.getElementById('gambar-merk');
+    
+    if(x.style.display == 'none'){
+        x.style.display = 'block';
+    }else{
+        x.style.display = 'none';
+    }
+});
+// addeventlistener show image merk end
+
+//pop up box start
+document.querySelector('.input-button').addEventListener("click",function cari(){
+    alert("Maaf Barang Kosong !!!");
+});
+
+
---
+++ postest4/logout.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+session_destroy();
+
+echo"
+<script>
+    alert('Anda Berhasil Logout!!!');
+    window.location.href=('index.php');
+</script>
+
+
+";
+
+?>
